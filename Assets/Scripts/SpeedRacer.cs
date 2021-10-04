90fe995e856e80b1974e8aded8bf23f953ded6c7
Thijs <thijs@uni-bremen.de>
2021-10-04 22:30:54 +0300

    Setting up Varaibles

--- Assets/Scripts/SpeedRacer.cs
+++ Assets/Scripts/SpeedRacer.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 public class SpeedRacer : MonoBehaviour
 {
+    private string carModel = "GTR R35";
+    private string engineType = "V6, Twin Turbo";
+    private int carWeight = 1609;
+    private int yearMade = 2009;
+    private float maxAcceleration = 0.98f;
+    private bool isCarTypeSedan = false;
+    private bool hasFrontEngine = true;
 
     // Start is called before the first frame update
     void Start()
