1311575e2022df416d8723725b7aefbfef1f4073
mockba1501 <7790976+mockba1501@users.noreply.github.com>
2021-10-05 14:34:04 +0300

    Update SpeedRacer.cs
    Variable Declarations

--- Assets/Scripts/SpeedRacer.cs
+++ Assets/Scripts/SpeedRacer.cs
@@ -2,6 +2,14 @@ using UnityEngine;
 
 public class SpeedRacer : MonoBehaviour
 {
+    //variable declaration
+    public string carModel = "GTR R35";
+    public string engineType = "V6, Twin Turbo";
+    public int carWeight = 1609;
+    public int yearMade = 2009;
+    public float maxAcceleration = 0.98f;
+    public bool isCarTypeSedan = false;
+    public bool hasFrontEngine = true;
 
     // Start is called before the first frame update
     void Start()
