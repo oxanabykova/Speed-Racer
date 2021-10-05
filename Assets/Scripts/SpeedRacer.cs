a4ef6201e1c2244c7eedaf1582ca02548947865f
mockba1501 <7790976+mockba1501@users.noreply.github.com>
2021-10-05 14:55:29 +0300

    Added CheckWeight & Calculate Age functions
    Added the following:
    - print message
    - CheckWeight function + its function call
    - CalculateAge function

--- Assets/Scripts/SpeedRacer.cs
+++ Assets/Scripts/SpeedRacer.cs
@@ -14,12 +14,30 @@ public class SpeedRacer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //print car model and engine type
+        print("The racer model is " + carModel + ". It has a " + engineType + " engine.");
+        CheckWeight();
     }
 
     // Update is called once per frame
     void Update()
     {
        
+    }
+
+    //Check if weight of the car is less or more than 1500
+    void CheckWeight()
+    {
+        if (carWeight < 1500)
+            print("the " + carModel + " weighs less than 1500 kg.");
+        else
+            print("the " + carModel + " weighs over 1500 kg.");
+    }
+
+    //Calculate the age of the car from the current year 2021
+    //The function variable name is different from the global variable name yearMade because not best practice to use the same varaible for both local and public variables
+    int CalculateAge(int year)
+    {
+        return 2021 - year;
     }
 }
